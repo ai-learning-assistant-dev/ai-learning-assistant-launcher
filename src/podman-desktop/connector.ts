ed1fefb09a1fbf6448906757f7f01a811aec84b7
James Zhang <shiftonetothree@126.com>
2025-06-14 14:19:06 +0800

    feat(*): add podman-desktop

---
+++ src/podman-desktop/connector.ts
@@ -0,0 +1,53 @@
+import { isMac, isWindows } from "./util";
+import { extensionApi } from './real-api';
+import { LibPod, LibpodDockerode } from "./libpod-dockerode";
+import Dockerode from "dockerode";
+
+export const macosExtraPath = '/opt/podman/bin:/usr/local/bin:/opt/homebrew/bin:/opt/local/bin';
+
+export function getInstallationPath(envPATH?: string): string {
+  envPATH ??= process.env['PATH'];
+
+  if (isWindows()) {
+    return `c:\\Program Files\\RedHat\\Podman;${envPATH}`;
+  } else if (isMac()) {
+    if (!envPATH) {
+      return macosExtraPath;
+    } else {
+      return macosExtraPath.concat(':').concat(envPATH);
+    }
+  } else {
+    return envPATH ?? '';
+  }
+}
+
+export function getPodmanCli(): string {
+
+  if (isWindows()) {
+    return 'podman.exe';
+  }
+  return 'podman';
+}
+
+async function getSocketPath(machineName: string): Promise<string> {
+  let socketPath: string = "";
+  const { stdout: socket } = await extensionApi.process.exec(getPodmanCli(), [
+    'machine',
+    'inspect',
+    '--format',
+    '{{.ConnectionInfo.PodmanPipe.Path}}',
+    machineName,
+  ]);
+  socketPath = socket;
+  return socketPath
+}
+
+const libPodDockerode = new LibpodDockerode();
+libPodDockerode.enhancePrototypeWithLibPod();
+
+export async function connect(machineName: string = "podman-machine-default"): Promise<LibPod> {
+  const socketPath = await getSocketPath(machineName);
+  console.debug("Socket Path: ", socketPath);
+  return new Dockerode({ socketPath: socketPath }) as unknown as LibPod;
+}
+
---
+++ src/podman-desktop/real-api.ts
@@ -0,0 +1,15 @@
+import type * as containerDesktopAPI from '@podman-desktop/api';
+import { Exec } from './exec';
+const exec = new Exec();
+
+export const extensionApi = {
+  process: {
+    exec: (
+      command: string,
+      args?: string[],
+      options?: containerDesktopAPI.RunOptions,
+    ): Promise<containerDesktopAPI.RunResult> => {
+      return exec.exec(command, args, options);
+    }
+ } as typeof containerDesktopAPI.process 
+}
