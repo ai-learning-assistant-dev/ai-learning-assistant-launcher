5382d21c552c895c99d04e2d0009d6e67830df7f
xiaoshen <yvbopeng@163.com>
2025-11-29 10:02:22 +0800

    fix: update CSP policy to support captcha verification

--- src/main/training-service/index.tsx
+++ src/main/training-service/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserWindow, IpcMain } from 'electron';
+import { BrowserWindow, session, IpcMain } from 'electron';
 import {
   installTrainingServiceHandle,
   removeTrainingServiceHandle,
@@ -67,6 +67,38 @@ const createWindow = (): void => {
   trainingWindow = new BrowserWindow({
     height: 900,
     width: 1400,
+  });
+
+  // 修改CSP以允许获取验证码
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    if (
+      details.url.startsWith(trainingWebURL) ||
+      details.url.includes('geetest.com') ||
+      details.url.includes('geevisit.com')
+    ) {
+      const customCSP = [
+        "default-src 'self' http://127.0.0.1:7100 blob: data:",
+        "script-src 'self' 'unsafe-inline' blob: data: https://api.geetest.com https://static.geetest.com https://monitor.geetest.com https://static.geevisit.com",
+        "connect-src 'self' blob: data: https://api.geetest.com https://static.geetest.com https://monitor.geetest.com https://static.geevisit.com",
+        "style-src 'self' 'unsafe-inline' https://static.geetest.com https://static.geevisit.com",
+        "img-src 'self' data: blob: https://github.com https://*.github.com https://*.githubusercontent.com https://static.geetest.com https://static.geevisit.com",
+        "media-src 'self' blob: data: http://127.0.0.1:7100 https://static.geetest.com https://static.geevisit.com",
+        "font-src 'self' data:",
+        "object-src 'none'",
+        "base-uri 'self'",
+        "frame-src 'self' blob: data: https://*.geetest.com",
+        "worker-src 'self' blob: data:",
+      ].join('; ');
+
+      callback({
+        responseHeaders: {
+          ...details.responseHeaders,
+          'Content-Security-Policy': [customCSP],
+        },
+      });
+    } else {
+      callback({ responseHeaders: details.responseHeaders });
+    }
   });
 
   trainingWindow.loadURL(trainingWebURL);
