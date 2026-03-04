47a29b97539851aba13cf74b5eee7482bad340ff
James Zhang <shiftonetothree@126.com>
2026-03-04 14:22:52 +0000

    chore(forge.config.ts): ignore zip

--- forge.config.ts
+++ forge.config.ts
@@ -77,6 +77,7 @@ const config: ForgeConfig = {
         '*.tar.gz',
         '*.tar',
         '*.mp4',
+        '*.zip',
       ];
       if (process.env.MAKE_MINI) {
         bigFileSuffix.forEach((suffix) => {
