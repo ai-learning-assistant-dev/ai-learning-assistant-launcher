84979eea1a4866a560713e416b9591915d7565b4
James Zhang <shiftonetothree@126.com>
2026-04-03 21:48:08 +0800

    chore(package.json): change version

--- src/main/dlc/index.ts
+++ src/main/dlc/index.ts
@@ -185,7 +185,7 @@ export default async function init(ipcMain: IpcMain) {
 
   // 尝试更新DLC索引文件，失败时只记录错误日志，不向外抛出异常
   try {
-    // await updateDLCIndex();
+    await updateDLCIndex();
   } catch (error) {
     console.error('更新DLC索引失败:', error);
   }
